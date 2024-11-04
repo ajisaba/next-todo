ba495679d1b6b1f46dd0bdddfd866240807ed1d9
ajisaba <aji7saba@gmail.com>
2024-11-04 23:41:57 +0900

    Initial commit

---
+++ test/next-todo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NextTodo from '../lib/next-todo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/next-todo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NextTodo.NextTodoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
