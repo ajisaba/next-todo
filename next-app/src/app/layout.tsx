84a1df7a2a4a3a50d7eaf6fc921fd0e110a76e1c
ajisaba <aji7saba@gmail.com>
2025-02-24 14:15:11 +0900

    Remove unnecessary fonts

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -1,21 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import clsx from "clsx";
 import Link from "next/link";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
-const cnTopItem = clsx("h-full p-2 border-r-2 border-white border-solid align-middle");
+const cnTopItem = clsx(
+  "h-full p-2 border-r-2 border-white border-solid align-middle"
+);
 
 export const metadata: Metadata = {
   title: "Next-Todo",
@@ -29,12 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <header className="h-[40px] w-full bg-blue-500 flex items-center text-white font-bold">
           <div id="top-menu" className={cnTopItem}>
-            <Link id="top-menu-home" href="/">Home</Link>
+            <Link id="top-menu-home" href="/">
+              Home
+            </Link>
           </div>
         </header>
         {children}
